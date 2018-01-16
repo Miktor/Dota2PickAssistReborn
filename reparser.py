0950fda8c9a4f3628e0d3dabcb674c7997399d74
Gladky Dmitry <miktor.ru@gmail.com>
2018-01-16 23:51:44 +0300

    add reoarser

---
+++ reparser.py
@@ -0,0 +1,63 @@
+import logging
+import json
+import glob
+import os
+
+
+def reparse(data):
+    try:
+        if data['human_players'] is not 10:
+            return
+
+        packed = {'match_id': data['match_id'],
+                  'region': data['region'],
+                  'start_time': data['start_time'],
+                  'version': '{}.{}'.format(data['version'], data['patch']),
+                  'radiant_win': data['radiant_win']}
+
+        heroes = []
+        for player in data['players']:
+            heroes.append({'hero_id': player['hero_id'],
+                           'isRadiant': player['isRadiant']})
+
+        packed['heroes'] = heroes
+
+        return packed
+    except Exception:
+        logging.exception('Failed to pack')
+
+
+def main():
+    SAVE_FOLDER = 'matches'
+    SAVE_FILE = 'packed.json'
+
+    try:
+        list_of_files = glob.glob(SAVE_FOLDER + '/*.json')
+        latest_file = max(list_of_files, key=os.path.getctime)
+
+        logging.info('Found %u files', len(list_of_files))
+
+        out_data = []
+        for filename in list_of_files:
+            with open(filename, 'rt') as in_file:
+                try:
+                    data = json.loads(in_file.read())
+                    if data:
+                        res = reparse(data)
+                        if res:
+                            out_data.append(res)
+
+                except json.decoder.JSONDecodeError:
+                    pass
+                except Exception as e:
+                    logging.exception('Failed to read file %s', filename)
+
+        with open(SAVE_FILE, 'wt') as out_file:
+            json.dump(out_data, out_file)
+
+    except Exception as e:
+        logging.exception('Failed to enumerate files')
+
+
+if __name__ == '__main__':
+    main()
