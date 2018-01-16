36f0ff51e6c98fe4d6b79ddbbd9b2d6c64b4fc44
Boris Shishov <borisshishov@gmail.com>
2018-01-16 01:39:14 +0100

    Gitignore, naming

--- scrabber.py
+++ scrabber.py
@@ -10,20 +10,20 @@ import urllib.request
 import urllib.parse
 
 
-def query_matches(startingMatchId, minMMR):
+def query_matches(starting_match_id, min_mmr):
     try:
-        sqlQuery = ("select * from public_matches "
+        sql_query = ("select * from public_matches "
                     "where avg_mmr >= {0} and match_id > {1} "
                     "and lobby_type = 7 and game_mode in (2, 22) "
                     "ORDER BY start_time asc "
                     "limit 100")
 
         query = "https://api.opendota.com/api/explorer?sql=" + urllib.parse.quote(
-            sqlQuery.format(minMMR, startingMatchId))
+            sql_query.format(min_mmr, starting_match_id))
 
         logging.debug(
-            'Quering match from mmr %d, min match id = %d, string = %s', minMMR,
-            startingMatchId, query)
+            'Quering match from mmr %d, min match id = %d, string = %s', min_mmr,
+            starting_match_id, query)
 
         response = urllib.request.urlopen(query)
 
@@ -32,10 +32,10 @@ def query_matches(startingMatchId, minMMR):
         rows = data['rows']
         matches = []
         for row in rows:
-            matchID = row['match_id']
-            matches.append(matchID)
+            match_id = row['match_id']
+            matches.append(match_id)
             logging.debug(
-                'match_id = %d, mmr = %d, date = %s', matchID, row['avg_mmr'],
+                'match_id = %d, mmr = %d, date = %s', match_id, row['avg_mmr'],
                 str(datetime.datetime.fromtimestamp(row['start_time'])))
         return matches
     except Exception as e:
@@ -43,10 +43,10 @@ def query_matches(startingMatchId, minMMR):
         return []
 
 
-def query_match(matchId):
+def query_match(match_id):
     try:
-        query = "https://api.opendota.com/api/matches/{}".format(matchId)
-        logging.debug('Quering match %d, string = %s', matchId, query)
+        query = "https://api.opendota.com/api/matches/{}".format(match_id)
+        logging.debug('Quering match %d, string = %s', match_id, query)
 
         response = urllib.request.urlopen(query)
 
@@ -57,8 +57,8 @@ def query_match(matchId):
 
 
 def main():
-    firstMatch = 3534655441
-    saveFolder = 'matches'
+    first_match = 3534655441
+    save_folder = 'matches'
 
     with open('logging.json', 'rt') as configFile:
         config = json.load(configFile)
@@ -67,39 +67,39 @@ def main():
     logging.info('Start')
 
     try:
-        os.stat(saveFolder)
+        os.stat(save_folder)
         try:
-            list_of_files = glob.glob(saveFolder + '/*.json')
+            list_of_files = glob.glob(save_folder + '/*.json')
             latest_file = max(list_of_files, key=os.path.getctime)
 
-            lastMatch = int(
-                latest_file[len(saveFolder) + 1:-(len('json') + 1)])
-            logging.info('Found last saved match %u', lastMatch)
-            if firstMatch < lastMatch:
+            last_match = int(
+                latest_file[len(save_folder) + 1:-(len('json') + 1)])
+            logging.info('Found last saved match %u', last_match)
+            if first_match < last_match:
                 logging.warning('Overriding first match from %u to %u',
-                                firstMatch, lastMatch)
-                firstMatch = lastMatch
+                                first_match, last_match)
+                first_match = last_match
         except Exception as e:
             logging.exception('Failed to enumerate files')
     except:
-        logging.info('Creating folder \'%s\'', saveFolder)
-        os.mkdir(saveFolder)
+        logging.info('Creating folder \'%s\'', save_folder)
+        os.mkdir(save_folder)
 
     while True:
-        for match in query_matches(firstMatch, 4000):
+        for match in query_matches(first_match, 4000):
             try:
                 logging.debug('Quering match %u', match)
-                matchData = query_match(match)
+                match_data = query_match(match)
 
-                filePath = saveFolder + '/' + str(match) + '.json'
-                with open(filePath, 'w') as outfile:
+                file_path = save_folder + '/' + str(match) + '.json'
+                with open(file_path, 'w') as outfile:
                     json.dump(
-                        matchData,
+                        match_data,
                         outfile,
                         sort_keys=True,
                         indent=4,
                         separators=(',', ': '))
-                    logging.debug('saved match %d to %s', match, filePath)
+                    logging.debug('saved match %d to %s', match, file_path)
                 time.sleep(0.25)
             except Exception as e:
                 logging.exception('Failed to enumerate files')
