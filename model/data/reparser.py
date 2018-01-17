35502a82ea2b925a945aab08325c241f9f1c055c
Gladky Dmitry <miktor.ru@gmail.com>
2018-01-18 02:46:21 +0300

    reparser

--- model/data/reparser.py
+++ model/data/reparser.py
@@ -2,27 +2,78 @@ import logging
 import json
 import glob
 import os
+import operator
 
-MATCHES_FOLDER = 'D:\matches\matches\matches'
+MATCHES_FOLDER = 'raw'
 SAVE_FILE = 'packed.json'
+
+unparsed = 0
+total = 0
+leaved = 0
+radiant_wins = 0
+dire_winds = 0
+avg_mmr = 0
+heroes_picked_times = {}
 
 
 def reparse(data):
+    global unparsed
+    global total
+    global leaved
+    global radiant_wins
+    global dire_winds
+    global avg_mmr
+    global heroes_picked_times
+
     try:
-        if data['human_players'] is not 10:
-            logging.exception('human_players = %d', data['human_players'])
+        if 'version' not in data or data['version'] is None:
+            logging.info('unparsed')
+            unparsed += 1
             return
 
+        if data['human_players'] is not 10:
+            logging.info('human_players = %d', data['human_players'])
+            return
+
+        if data['duration'] < 10 * 60:
+            logging.info('duration < 10 min')
+            return
+
+        for player in data['players']:
+            if player['leaver_status'] == 1:
+                logging.info('leaver')
+                leaved += 1
+                return
+
+        radiant_win = data['radiant_win']
+        mmr = data['mmr']
         packed = {'match_id': data['match_id'],
-                  'region': data['region'],
+                  'mmr': mmr,
                   'start_time': data['start_time'],
-                  'version': '{}.{}'.format(data['version'], data['patch']),
-                  'radiant_win': data['radiant_win']}
+                  'duration': data['duration'],
+                  'radiant_win': radiant_win}
+
+        total += 1
+        if radiant_win:
+            radiant_wins += 1
+        else:
+            dire_winds += 1
+        avg_mmr += mmr
 
         heroes = []
         for player in data['players']:
-            heroes.append({'hero_id': player['hero_id'],
-                           'isRadiant': player['isRadiant']})
+            hero_id = player['hero_id']
+            heroes.append({'hero_id': hero_id,
+                           'isRadiant': player['isRadiant'],
+                           'lane_efficiency': player['lane_efficiency'],
+                           'lane': player['lane'],
+                           'lane_role': player['lane_role'],
+                           'is_roaming': player['is_roaming']
+                           })
+            if hero_id in heroes_picked_times:
+                heroes_picked_times[hero_id] += 1
+            else:
+                heroes_picked_times[hero_id] = 1
 
         packed['heroes'] = heroes
 
@@ -33,10 +84,14 @@ def reparse(data):
 
 def main():
     try:
+        FORMAT = '%(asctime)-15s %(clientip)s %(user)-8s %(message)s'
+
+        # logging.basicConfig(format=FORMAT, level=logging.DEBUG)
+
         list_of_files = glob.glob(MATCHES_FOLDER + '/*.json')
         latest_file = max(list_of_files, key=os.path.getctime)
 
-        logging.info('Found %u files', len(list_of_files))
+        logging.info('Found %d files', len(list_of_files))
 
         out_data = []
         for filename in list_of_files:
@@ -62,3 +117,11 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+    print('Stats: unparsed = {}, total = {}, leaved = {}, radiant: {}, dire: {}, mmr: {}, heroes: {}'.format(unparsed,
+                                                                                                             total,
+                                                                                                             leaved,
+                                                                                                             radiant_wins,
+                                                                                                             dire_winds,
+                                                                                                             avg_mmr / total,
+                                                                                                             sorted(heroes_picked_times.items(), key=operator.itemgetter(1), reverse=True)))
