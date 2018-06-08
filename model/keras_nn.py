782f76f89f90b68be1c88b79f73fa9c474146510
Boris Shishov <borisshishov@gmail.com>
2018-06-08 16:26:58 +0200

    Adjusted keras model. Changed loss, and added fit callbacks

--- model/keras_nn.py
+++ model/keras_nn.py
@@ -1,4 +1,4 @@
-from keras import Sequential
+import keras
 from keras.layers import Dense, Dropout
 from keras.optimizers import Adam
 
@@ -9,18 +9,26 @@ class KerasModel(object):
 
     def __init__(self, pick_shape: tuple, pick_game_shape: tuple, pick_game_num_actions: int):
 
-        model = Sequential()
+        model = keras.Sequential()
         model.add(Dense(512, activation='relu', input_shape=(pick_shape[0],)))
         model.add(Dropout(0.5))
         model.add(Dense(512, activation='relu'))
         model.add(Dropout(0.5))
         model.add(Dense(1, activation='sigmoid'))
 
-        model.compile(loss='sparse_categorical_crossentropy', optimizer=Adam(lr=0.001), metrics=['accuracy'])
+        model.compile(loss='binary_crossentropy', optimizer=Adam(lr=1e-4), metrics=['accuracy'])
         self.model = model
 
-    def train(self, picks, results, epochs=10):
-        self.model.fit(picks, results, batch_size=BATCH_SIZE, epochs=epochs)
+    def train(self, picks, results, max_epochs=1000):
+        callbacks = [
+            keras.callbacks.EarlyStopping(monitor='val_loss', patience=20),
+            keras.callbacks.TerminateOnNaN()
+        ]
+        self.model.fit(picks, results,
+                       batch_size=BATCH_SIZE,
+                       epochs=max_epochs,
+                       validation_split=0.2,
+                       callbacks=callbacks)
 
     def predict(self, inputs):
         return self.model.predict(inputs)
